69953b2d3927213ce7c05de6e71e94b193644445
Emma-Barash <emma.barash.dev@gmail.com>
2019-07-24 15:55:37 -0600

    style

--- src/elements/CompContainers.js
+++ src/elements/CompContainers.js
@@ -2,5 +2,7 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
     grid-area: h;
+    text-align: center;
+    padding: 25px;
     border: 1px solid blue;
 `;
--- src/elements/MainGrid.js
+++ src/elements/MainGrid.js
@@ -4,12 +4,13 @@ export const GridPage = styled.div`
     display: grid;
     width: 100vw;
     height: auto;
+    position: absolute;
+    top: 0;
     margin: 0;
     padding: 0;
     background-image: linear-gradient(white, white);
     grid-template-columns: repeat(4, auto);
     grid-template-rows: repeat(4, auto);
-    background-size: fill;
     grid-template-areas: 
         "h h h h"
     ;
